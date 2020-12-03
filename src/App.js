df7b7ae2fa21eee713337a5912eeb34880972e43
suthicha <suthicha@outlook.com>
2020-12-03 12:45:57 +0700

    Day1: How to build react project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    username: "",
+    password: "",
+    result: ""
+  };
+
+  handleInput = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+    console.log(name, value);
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.setState({
+      result: this.state.username + '/' + this.state.password
+    });
+  }
+
+  render() {
+    //TODO
+    return (
+      <div className="App">
+        <input
+          name="username"
+          placeholder="Please enter Username"
+          maxLength="2"
+          value={this.state.username}
+          onChange={this.handleInput}/>
+          <br />
+        <input
+          name="password"
+          onChange={this.handleInput}/>
+          <br />
+        <button
+          onClick={this.handleSubmit}>
+            Submit
+        </button>
+    <div>{ this.state.result }</div>
+        {/* <br />
+        <label>Username: {this.state.username}</label>
+        <br />
+        <label>Password: {this.state.password}</label> */}
+
+      </div>
+    );
+  }
 }
-
 export default App;
